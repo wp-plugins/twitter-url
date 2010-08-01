712237ab58af92736dd300b1ffc2176590113232
Parkeli <Parkeli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-01 05:28:56 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/twitter-url/trunk@270172 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Twitter-URL.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Twitter URL
+Plugin URI: http://scripts.liamparker.com/Wordpress/Wordpress-Plugins/Twitter-URL.php
+Description: A Wordpress plugin that replaces Twitter @replies and #hashtags with the appropriate Twitter links.
+Version: 1.0
+Author: LMP
+Author URI: http://liamparker.com/
+*/
+function hashTags($matches){
+$hash = $matches[2];
+$result = ("<a href='http://twitter.com/search?q=$hash'>#$hash</a>");
+return($result);
+}
+function twitterUsernames($matches){
+$username= $matches[2];
+$result = ("<a href='http://twitter.com/$username'>@$username</a>");
+return($result);
+}
+function twitterURL($content){
+$content = preg_replace_callback('/(^|[^a-z0-9_])@([a-z0-9_]+)/i','twitterUsernames',$content);
+$content = preg_replace_callback('/(^|[^a-z0-9_])#([a-z0-9_]+)/i','hashTags',$content);
+return $content;
+}
+add_filter('the_content', 'twitterURL');
+?>
