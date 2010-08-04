bbe84ec15aeec7e91a3bd2441a6c2aeddfaeab19
Parkeli <Parkeli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-04 05:11:14 +0000

    1.1
    git-svn-id: https://plugins.svn.wordpress.org/twitter-url/trunk@271474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Twitter-URL.php
+++ Twitter-URL.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: Twitter URL  
+Plugin Name: Twitter URL
 Description: A Wordpress plugin that replaces Twitter @replies and #hashtags with the appropriate Twitter links.
-Version: 1.0
+Version: 1.1
 Author: LMP
 Author URI: http://liamparker.com/
 */
 function hashTags($matches){
-$hash = $matches[2];
+$hash = $matches[1];
 $result = ("<a href='http://twitter.com/search?q=$hash'>#$hash</a>");
 return($result);
 }
@@ -18,7 +18,7 @@ return($result);
 }
 function twitterURL($content){
 $content = preg_replace_callback('/(^|[^a-z0-9_])@([a-z0-9_]+)/i','twitterUsernames',$content);
-$content = preg_replace_callback('/(^|[^a-z0-9_])#([a-z0-9_]+)/i','hashTags',$content);
+$content = preg_replace_callback('/\B(?<![=\/])#([\w]+[a-z]+([0-9]+)?)/i','hashTags',$content);
 return $content;
 }
 add_filter('the_content', 'twitterURL');
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Tags: Twitter, @replies, hashtags, URL
 Contributors: Liam Parker
 Requires at least: 2.7
 Tested up to: 3.0
-Stable Tag: 1.0
+Stable Tag: 1.1
 
 == Description ==
 
@@ -11,4 +11,5 @@ A Wordpress plugin that replaces Twitter @replies and #hashtags with the appropr
 
 == Change Log ==
 
-1.0 * First release
+1.0 * First release  
+1.1 * Bug fixes 
